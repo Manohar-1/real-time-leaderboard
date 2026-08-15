5ded9fd40e846775552766072597feb6817c9e9e
Manohar-1 <mudhapakamanohar@gmail.com>
2026-08-15 22:42:34 +0530

    added Unique Constraint for user and game

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, UniqueConstraint
+from sqlalchemy.orm import relationship
 
 from app.database import Base
 
@@ -9,14 +10,23 @@ class User(Base):
     username = Column(String, unique=True)
     email = Column(String, unique=True)
     password = Column(String)
-    score = Column(Integer,default=0)
+     
+    #back_populates referes to the "user" attribute in the Score model
+    scores = relationship("Score", back_populates="user")
 
 class Score(Base):
     __tablename__ = "scores"
+
+     __table_args__ = (
+        UniqueConstraint("user_id", "game", name="unique_user_game"),
+    )
 
     id= Column(Integer, primary_key=True)
     user_id = Column(Integer,ForeignKey('users.id'),nullable=False)
     game = Column(String, nullable=False)
     score = Column(Integer, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
+
+    #back_populates refers to the "scores" attribute in the User model
+    user = relationship("User", back_populates="scores")
 
