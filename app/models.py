7c40ee68517562dd0737eaa4415f3cf9e4c314ec
Manohar-1 <mudhapakamanohar@gmail.com>
2026-08-15 22:13:37 +0530

    added score table code

--- app/models.py
+++ app/models.py
@@ -10,3 +10,13 @@ class User(Base):
     email = Column(String, unique=True)
     password = Column(String)
     score = Column(Integer,default=0)
+
+class Score(Base):
+    __tablename__ = "scores"
+
+    id= Column(Integer, primary_key=True)
+    user_id = Column(Integer,ForeignKey('users.id'),nullable=False)
+    game = Column(String, nullable=False)
+    score = Column(Integer, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
